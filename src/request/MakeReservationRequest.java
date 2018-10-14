a1fe4bbab18716e74724d880cab91d28ea6c55a2
Giselle Cole <gjc4443@g.rit.edu>
2018-10-14 16:43:31 -0400

    Make and Delete Reservation Requests have implementation in them.

--- src/request/MakeReservationRequest.java
+++ src/request/MakeReservationRequest.java
@@ -3,6 +3,13 @@ package request;
 public class MakeReservationRequest implements Request {
 
     //TODO Read up on requirements for this request
+    private int id;
+    private String passenger;
+
+    public MakeReservationRequest(int id, String passenger){
+        this.id = id;
+        this.passenger = passenger;
+    }
 
     @Override
     public void execute() {
