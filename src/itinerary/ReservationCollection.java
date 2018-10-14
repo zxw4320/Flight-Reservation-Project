482c011d0cf4379a77813653ad9f74bb889d1ad6
Nitepone <admin@night.horse>
2018-10-14 15:00:34 -0400

    Added Reservation collection

--- src/itinerary/ReservationCollection.java
+++ src/itinerary/ReservationCollection.java
@@ -1,5 +1,41 @@
 package itinerary;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReservationCollection {
+
+    List<Reservation> reservations;
+
+    public ReservationCollection(){
+        reservations = new ArrayList<>();
+    }
+
+    /**
+     * Adds reservation to collection.
+     * @param reservation
+     */
+    public void addReservation(Reservation reservation){
+        reservations.add(reservation);
+    }
+
+    public void deleteReservation(Reservation reservation){
+        reservations.remove(reservation);
+    }
+
+    public List<Reservation> findReservation(String passenger){
+
+        List<Reservation> foundReservations = new ArrayList<>();
+
+        // search all reservations for substring
+        reservations.forEach(reservation -> {
+            if( reservation.getPassenger() == passenger ){
+                foundReservations.add(reservation);
+            }
+        });
+
+        return foundReservations;
+
+    }
 
 }
