6b1a7457238287371c225b72a6ea35c6f1831b17
Nitepone <admin@night.horse>
2018-10-14 22:45:31 -0400

    Work on ItineraryHistory

--- src/model/ItineraryHistory.java
+++ src/model/ItineraryHistory.java
@@ -1,4 +1,33 @@
 package model;
 
+import ui.AFRSInterface;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class ItineraryHistory {
+
+    HashMap<AFRSInterface, List<Itinerary>> itineraryHashMap;
+
+    public ItineraryHistory(){
+        itineraryHashMap = new HashMap<>();
+    }
+
+    public Itinerary getItinerary(AFRSInterface ui, int id){
+        // get the list from the hashmap
+        List<Itinerary> itineraryList = itineraryHashMap.get(ui);
+
+        // return null if out of bounds
+        if(id < 0 || id >= itineraryList.size()){
+            return null;
+        }
+        else {
+            return itineraryList.get(id);
+        }
+    }
+
+    public void addItineraries(AFRSInterface ui, List<Itinerary> itineraryList){
+        itineraryHashMap.put(ui,itineraryList);
+    }
+
 }
