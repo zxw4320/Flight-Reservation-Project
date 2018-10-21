1ca90624fab45d7f7673f6f92a39d7e64ef3f182
Nitepone <admin@night.horse>
2018-10-21 13:40:25 -0400

    Example of some black box unit tests

--- test/ui/AFRSTest.java
+++ test/ui/AFRSTest.java
@@ -1,4 +1,52 @@
 package ui;
 
+import database.*;
+import model.*;
+import request.*;
+
+import org.junit.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class AFRSTest {
+
+    RequestHandler afrs;
+    FakeUI ui;
+
+    @Before
+    public void setup(){
+         // acquire DB files
+        Path a = Paths.get("csv/airports.csv");
+        Path f = Paths.get("csv/flights.csv");
+        Path w = Paths.get("csv/weather.csv");
+        Path d = Paths.get("csv/delay.csv");
+        Path r = Paths.get("csv/reservations.csv");
+        // make DB readers
+        Flightdb flightdb = new CSVdb(a,w,f,d);
+        Reservationdb reservationdb = new ReservationCSVParser(r);
+        // use DB readers
+        RouteMap routeMap = flightdb.generateRouteMap();
+        ReservationCollection reservationCollection = reservationdb
+                .generateReservationCollection(routeMap);
+        // create request handler
+        afrs = new RequestHandler(routeMap, reservationCollection);
+
+        ui = new FakeUI();
+    }
+
+    @Test
+    public void airportInfoTest(){
+        List<String> responces = ui.sendCommand(afrs, "airport,BOS;");
+        assertEquals("Boston (BOS) is currently partly cloudy, 70F with 30 minute delays.", responces.get(0));
+
+    }
+
+
+
 }
--- test/ui/FakeUI.java
+++ test/ui/FakeUI.java
@@ -1,4 +1,42 @@
 package ui;
 
-public class FakeUI {
+import request.RequestHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeUI implements AFRSInterface{
+
+    List<String> responceList;
+
+    public FakeUI(){
+        responceList = new ArrayList<>();
+    }
+
+    @Override
+    /**
+     * Adds a print response to our list
+     */
+    public void printString(String printText) {
+        responceList.add(printText);
+    }
+
+    protected List<String> sendCommand(RequestHandler requestHandler, String request){
+        requestHandler.makeRequest(this, request);
+
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        List<String> tempString = new ArrayList<>(responceList);
+        responceList.clear();
+        return tempString;
+
+    }
+
+
+
+
 }
