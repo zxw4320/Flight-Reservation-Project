c288f8560c344632e6a05cde885b89fa7b4ae6cd
Steve Sarnecki <svs5595@rit.edu>
2018-11-03 18:47:32 -0400

    Tabs initalized, tabs are created with loop currently

--- src/ui/GUI.java
+++ src/ui/GUI.java
@@ -2,23 +2,70 @@ package ui;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.awt.*;
 
 public class GUI extends Application implements MultiSessionUI{
     public static void main(String[] args){
-        launch(args);
+        Application.launch(args);
     }
 
     @Override
     public void start(Stage primaryStage){
         primaryStage.setTitle("Hello World");
+        Group root = new Group();
+        Scene scene = new Scene(root, 400, 250, Color.WHITE);
+        TabPane tabPane = new TabPane();
+        //Button addButton = new Button("+");
+
+        BorderPane borderPane = new BorderPane();
+        for (int i = 0; i < 5; i++) {
+            Tab tab = new Tab();
+            tab.setText("User" + i);
+            HBox hbox = new HBox();
+            hbox.getChildren().add(new Label("Tab"+i));
+            hbox.setAlignment(Pos.CENTER);
+            tab.setContent(hbox);
+
+            Button btn = new Button();
+            btn.setText("Say 'Hello World'");
+            btn.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    System.out.println("Hello World!");
+                }
+            });
+        }
+        // bind to take available space
+        borderPane.prefHeightProperty().bind(scene.heightProperty());
+        borderPane.prefWidthProperty().bind(scene.widthProperty());
+
+        borderPane.setCenter(tabPane);
+        root.getChildren().add(borderPane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+
+
+
+
+
+
+/*
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -27,12 +74,10 @@ public class GUI extends Application implements MultiSessionUI{
                 System.out.println("Hello World!");
             }
         });
-
-        StackPane root = new StackPane();
         root.getChildren().add(btn);
         primaryStage.setScene(new Scene(root, 300,250));
         primaryStage.show();
-    }
+        */
 
 
     @Override
