99580775777f70fbd7f112b4ecce2880938b8fc1
Steve Sarnecki <svs5595@rit.edu>
2018-11-03 17:56:00 -0400

    Demo GUI initialized, not yet connected to system backend.

--- src/ui/GUI.java
+++ src/ui/GUI.java
@@ -4,8 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class GUI extends Application implements MultiSessionUI{
     public static void main(String[] args){
