ca0ecec0506d408d97c9e3f4220da7ceb948358f
Steve Sarnecki <svs5595@rit.edu>
2018-11-04 01:25:18 -0500

    Implemented Listener for Map for use in UI updating.

---
+++ src/ui/ListenerModel.java
@@ -0,0 +1,21 @@
+package ui;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class ListenerModel {
+
+    private final PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
+    }
+
+    protected void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
+        changeSupport.firePropertyChange(propertyName, oldValue, newValue);
+    }
+}
